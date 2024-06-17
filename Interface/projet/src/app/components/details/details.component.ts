feb7939a07c5093be09380c974e5625a7f00d3c1
Epiiiix <luigi.giusiano@gmail.com>
2024-06-17 08:58:14 +0200

    Real-Time data

--- Interface/projet/src/app/components/details/details.component.ts
+++ Interface/projet/src/app/components/details/details.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SensorData } from '../../../types';
 import { DataService } from '../../service/data.service';
 import { BoxComponent } from '../box/box.component';
 import { UserService } from '../../service/user.service';
 import { Router } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css',
+  styleUrls: ['./details.component.css'], // Correction du styleUrls
   imports: [BoxComponent],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   sensorDatas: SensorData[] = [];
   lastSensorData: SensorData | undefined;
   averageTemperature: number = 0;
   averageLight: number = 0;
   averageDust: number = 0;
+  private pollingSubscription: Subscription | undefined;
 
   constructor(
     private dataService: DataService,
@@ -66,7 +69,30 @@ export class DetailsComponent implements OnInit {
     this.userService.isConnected().subscribe((isConnected) => {
       if (!isConnected) {
         this.router.navigate(['']);
-      } else this.fetchData('http://localhost:3000/get-data');
+      } else {
+        this.startPolling('http://localhost:3000/get-data');
+      }
     });
+  }
+
+  startPolling(url: string) {
+    this.pollingSubscription = interval(2000)
+      .pipe(switchMap(() => this.dataService.getData(url)))
+      .subscribe({
+        next: (data: SensorData[]) => {
+          this.sensorDatas = data;
+          this.lastSensorData = this.getLastSensorData(data);
+          this.calculateAverages(data);
+        },
+        error: (error) => {
+          console.error('Error fetching data:', error);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.pollingSubscription) {
+      this.pollingSubscription.unsubscribe();
+    }
   }
 }
